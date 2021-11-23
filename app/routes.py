1f60232fe1aa384c7f115f677db0a653861f48c6
katyaZhygulskaya <94320796+katyaZhygulskaya@users.noreply.github.com>
2021-11-23 23:42:43 +0300

    Add files via upload

--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,24 @@
-# -*- coding: utf-8 -*- 
-from app import app 
- 
-@app.route('/') 
-@app.route('/index') 
-def index(): 
-    return "Привет, Мир! Я Екатерина"
+# -*- coding: utf-8 -*-
+from flask import render_template
+from app import app
+
+
+@app.route('/')
+@app.route('/index')
+def index():
+    user = {'username': 'Katya'}
+    posts = [
+    {
+        'author': {'username': 'John'},
+        'body': 'Beautiful day in Portland!'
+    },
+    {
+        'author': {'username': 'Susan'},
+        'body': 'The Avengers movie was so cool!'
+    },
+    {
+        'author': {'username': 'Винни'},
+        'body': 'Не пора ли подкрепиться?'
+    }
+    ]
+    return render_template('index.html', title='Home', user=user, posts=posts)
